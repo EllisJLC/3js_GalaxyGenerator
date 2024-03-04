ceab18e45ebcd7cf19b67fcd87d0eff8fce98551
EllisJLC <jia.liang.chen@ryerson.ca>
2024-03-03 23:20:31 -0500

    Added randomness in spirals

--- src/script.js
+++ src/script.js
@@ -19,11 +19,31 @@ const scene = new THREE.Scene()
 const parameters = {}
 parameters.count = 1000
 parameters.size = 0.02
+parameters.radius = 5
+parameters.branches = 3
+parameters.spin = 1
+parameters.randomness = 0.2
+parameters.randomnessPower = 3
 
 gui.add(parameters, 'count').min(100).max(5000).step(100).onFinishChange( () => {
     generateGalaxy()
 })
 gui.add(parameters, 'size').min(0.001).max(0.3).step(0.001).onFinishChange( () => {
+    generateGalaxy()
+})
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.001).onFinishChange( () => {
+    generateGalaxy()
+})
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange( () => {
+    generateGalaxy()
+})
+gui.add(parameters, 'spin').min(-5).max(5).step(0.01).onFinishChange( () => {
+    generateGalaxy()
+})
+gui.add(parameters, 'randomness').min(0).max(2).step(0.01).onFinishChange( () => {
+    generateGalaxy()
+})
+gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange( () => {
     generateGalaxy()
 })
 
@@ -47,9 +67,17 @@ const generateGalaxy = () => {
 
         const i3 = i * 3
 
-        positions[i3 + 0] = Math.random() * 10 - 5
-        positions[i3 + 1] = Math.random() * 10 - 5
-        positions[i3 + 2] = Math.random() * 10 - 5
+        const radius = Math.random() * parameters.radius
+        const spinAngle = radius * parameters.spin
+        const branchAngle = i % parameters.branches / parameters.branches * 2 * Math.PI
+
+        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1: -1)
+        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1: -1)
+        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1: -1)
+
+        positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius + randomX
+        positions[i3 + 1] = randomY
+        positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ
         
     }
 
