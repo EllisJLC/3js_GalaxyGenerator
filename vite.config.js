19e247f75ed11c3b6552356e274bef7ecf834881
EllisJLC <97536806+EllisJLC@users.noreply.github.com>
2024-05-06 16:28:53 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: '/3js_GalaxyGenerator/',
+    base: './',
     server:
     {
         host: true, // Open to local network and display URL
